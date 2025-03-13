bc9bec5a3fc7a64d22f33d02f18d0bc87181c071
asha1928 <ashadurgam1928@gmail.com>
2025-03-13 11:50:02 +0530

    list_ tuple.py

--- tuple.py
+++ tuple.py
@@ -1,6 +1,4 @@
 from datetime import date
-
-
 employees = [
     (101, "Asha", "HR"),
     (102, "bhanu", "IT"),
@@ -8,12 +6,10 @@ employees = [
     (104, "arvind", "Marketing"),
 ]
 attendance_records = []
-
 def mark_attendance(emp_id, status):
     today = date.today().strftime("%Y-%m-%d")
     attendance_records.append((emp_id, today, status))
     print(f"Attendance marked: {emp_id} - {status}")
-
 def search_attendance(emp_id):
     print(f"\nAttendance for Employee ID: {emp_id}")
     found = False
@@ -23,9 +19,7 @@ def search_attendance(emp_id):
             found = True
     if not found:
         print("No attendance records found.")
-
 def display_summary():
-    """Displays attendance summary for all employees"""
     print("\nAttendance Summary:")
     for emp in employees:
         emp_id = emp[0]
@@ -36,6 +30,5 @@ mark_attendance(101, "Present")
 mark_attendance(102, "Absent")
 mark_attendance(103, "Present")
 mark_attendance(101, "Absent")
-
 search_attendance(101)
 display_summary()
